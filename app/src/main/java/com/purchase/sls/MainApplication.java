8b049c3b878e18fcd5648aa5efe64fbb5c6a200c
jwc <921577542@qq.com>
2018-05-26 10:32:35 +0800

    添加友盟

--- app/src/main/java/com/purchase/sls/MainApplication.java
+++ app/src/main/java/com/purchase/sls/MainApplication.java
@@ -1,10 +1,14 @@
 package com.purchase.sls;
 
 import android.app.Application;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.StrictMode;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
+import android.util.Log;
 
+import com.meituan.android.walle.WalleChannelReader;
 import com.purchase.sls.ApplicationComponent;
 import com.purchase.sls.ApplicationModule;
 import com.purchase.sls.DaggerApplicationComponent;
@@ -12,6 +16,7 @@ import com.purchase.sls.common.cityList.style.citylist.utils.CityListLoader;
 import com.purchase.sls.common.unit.NetUtils;
 import com.purchase.sls.common.unit.SPManager;
 import com.purchase.sls.data.local.GreenDaoModule;
+import com.umeng.analytics.MobclickAgent;
 
 import cn.jpush.android.api.JPushInterface;
 
@@ -29,6 +34,11 @@ public class MainApplication  extends MultiDexApplication {
         //Android7.0的照片问题
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
         StrictMode.setVmPolicy(builder.build());
+
+        //友盟统计
+        String channelId = WalleChannelReader.getChannel(this.getApplicationContext());
+        Log.d("1111","channelId="+channelId);
+        MobclickAgent. startWithConfigure(new MobclickAgent.UMAnalyticsConfig(getApplicationContext(),"5ab7102aa40fa357cb000ba3",channelId));
         /**
          * 预先加载一级列表所有城市的数据
          */
